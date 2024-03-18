a4f2796f21a0ea1a8676b15635cc3c7f6dd2eb3a
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2024-03-18 16:13:14 -0400

    Complete and test placeShip and receiveAttack

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,108 @@
+import Cell from "./cell";
 import Ship from "./ship";
 
 export default class Gameboard {
     constructor(player) {
-        // Make a grid with each cell having a coordinate property.
+        this.player = player;
+        this.letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        this.grid = [];
+        // Assigns alpha-numeric coordinates to each cell object in the grid
+        for(let i = 0; i < 10; i += 1) {
+            for(let j = 1; j < 11; j += 1) {
+                const currentCoordinate = `${this.letterArray[i]}${j}`;
+                this.grid.push(new Cell(currentCoordinate));
+            }
+        }
+        this.availableShips = [
+            {
+                name: "Carrier",
+                length: 5,
+            },
+            {
+                name: "Battleship",
+                length: 4,
+            },
+            {
+                name: "Cruiser",
+                length: 3,
+            },
+            {
+                name: "Submarine",
+                length: 3,
+            },
+            {
+                name: "Destroyer",
+                length: 2,
+            },
+        ];
+        this.cellsWithShips = [];
     }
     // Place ship function. Takes ship name, length, rotation, and coordinates and adds it to the grid.
+
+    placeShip(shipName, shipLength, coordinates, rotation) {
+        const currentShip = new Ship(shipName, shipLength);
+        const coordinateList = [];
+        const alpha = coordinates.charAt(0);
+        const numeric = parseInt(coordinates.charAt(1), 10);
+
+        if(rotation === "vertical") {
+            let startingIndex = this.letterArray.indexOf(alpha);
+        
+            if(startingIndex + currentShip.length > 10) {
+                throw Error("Ship placement extends out of range");
+            }
+
+            for(let i = 0; i < currentShip.length; i += 1) {
+                coordinateList.push(`${this.letterArray[startingIndex]}${numeric}`);
+                startingIndex += 1;
+            }
+
+        } else {
+            if(numeric + currentShip.length > 10) {
+                console.log(numeric + currentShip.length);
+                throw Error("Ship placement extends out of range");
+            }
+            
+            for(let i = numeric; i < currentShip.length + numeric; i += 1) {
+                coordinateList.push(`${alpha}${i}`);
+            }
+        }
+
+        coordinateList.forEach(coordinate => {
+            if(this.cellsWithShips.includes(coordinate)) {
+                throw Error("Ship placement overlaps another ship");
+            }
+        });
+
+        for(let j = 0; j < this.grid.length; j += 1) {
+            const cell = this.grid[j];
+            if(coordinateList.includes(cell.coordinates)) {
+                cell.hasShip = true;
+                cell.ship = currentShip;
+                this.cellsWithShips.push(cell.coordinates);
+            }
+        }
+
+    }
 
     // Random setup function. Places ships on the grid randomly until all ships are placed.
 
     // receiveAttack function needs to be able to call the hit function of a ship if present.
+    receiveAttack(coordinates) {
+        let currentCell;
+        this.grid.forEach(cell => {
+            if(cell.coordinates === coordinates) {
+                currentCell = cell;
+            }
+        });
+
+        if(currentCell.isHit) {
+            throw Error("This coordinate has already been hit");
+        }
+
+        currentCell.isHit = true;
+        if(currentCell.hasShip) {
+            currentCell.ship.hit();
+        }
+    }
 }
--- src/tests.test.js
+++ src/tests.test.js
@@ -1,5 +1,6 @@
 import Ship from "./ship";
 import Cell from "./cell";
+import Gameboard from "./gameboard";
 
 test("Returns a ship object that matches the example object", () => {
     const example = {
@@ -26,4 +27,62 @@ test("Creates a cell object with the coordinates property set to [5, 5]", () => 
     const cell = new Cell([5, 5]);
     const array = [5, 5];
     expect(cell.coordinates).toMatchObject(array);
+});
+
+test("Creates a gameboard and assigns alpha-numeric coordinates to each cell in the grid so that grid[0].coordinates is 'A1'", () => {
+    const board = new Gameboard("Test");
+    expect(board.grid[0].coordinates).toMatch("A1");
+});
+
+test("Places a Battleship vertically at B3", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.placeShip("Battleship", 4, "B3", "vertical");
+    let coord;
+    for(let i = 0; i < gameboard.grid.length; i += 1) {
+        if(gameboard.grid[i].coordinates === "E3") {
+            coord = gameboard.grid[i];
+        }
+    }
+
+    expect(coord.hasShip).toBe(true);
+});
+
+test("Places a Battleship horizontally at H1", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.placeShip("Battleship", 4, "H1", "horizontal");
+    let coord;
+    for(let i = 0; i < gameboard.grid.length; i += 1) {
+        if(gameboard.grid[i].coordinates === "H4") {
+            coord = gameboard.grid[i];
+        }
+    }
+
+    expect(coord.hasShip).toBe(true);
+});
+
+test("Places a Battleship horizontally at A1 and attemps to place a Destroyer vertically at A3, result in an error", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.placeShip("Battleship", 4, "A1", "horizontal");
+    expect(() => gameboard.placeShip("Destroyer", 3, "A3", "vertical")).toThrow(Error);
+});
+
+test("Attacks a coordinate twice, resulting in an error", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.receiveAttack("A1");
+    expect(() => gameboard.receiveAttack("A1")).toThrow(Error);
+});
+
+test("Attacks a coordinate, resulting in isHit === true", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.receiveAttack("A1");
+    expect(gameboard.grid[0].isHit).toBe(true);
+});
+
+test("Places a ship, attacks and sinks the ship, result in ship.isSunk === true", () => {
+    const gameboard = new Gameboard("Test");
+    gameboard.placeShip("Destroyer", 3, "A1", "horizontal");
+    gameboard.receiveAttack("A1");
+    gameboard.receiveAttack("A2");
+    gameboard.receiveAttack("A3");
+    expect(gameboard.grid[0].ship.isSunk).toBe(true);
 });
